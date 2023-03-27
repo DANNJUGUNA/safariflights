89c27440d216438ada057ba770531a19af6162a4
mknyambura <kariuki.mercie@gmail.com>
2023-03-27 21:36:00 +0300

    update models

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     def index
-        users = Users.all
+        users = User.all
         render json: users
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_27_181209) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_27_182625) do
   create_table "bookings", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "flight_id", null: false

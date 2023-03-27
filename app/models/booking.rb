70889b03ff042321343dc3cb6f161cdb33d842fa
mknyambura <kariuki.mercie@gmail.com>
2023-03-27 19:54:12 +0300

    update models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,4 @@
 class Booking < ApplicationRecord
-  belongs_to :flight_id
-  belongs_to :user_id
+  belongs_to :user
+  belongs_to :flight
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,4 @@
 class Flight < ApplicationRecord
+    has_many :users
+    has_many :users, through: :bookings
 end

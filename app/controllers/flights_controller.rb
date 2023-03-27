bdcdf1e75ccd68e97b377fc203b3e1e8c83f4d3e
mknyambura <kariuki.mercie@gmail.com>
2023-03-27 23:41:08 +0300

    update flights controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,15 @@
 class FlightsController < ApplicationController
+    def index
+        flights = Flight.all
+        render json: flights, only: [:destination, :from, :duration, :departure, :cost]
+    end
+
+    def show
+        flight = Flight.find_by(id: params[:id])
+        if flight
+            render json: flight.as_json(includes: :users)
+        else
+            render json: { error: "Flight Not found" }, status: :not_found
+        end
+    end
 end

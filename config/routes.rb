e6bdec20f5c8975387c7a3995f8a990994d1486c
DAN NJUGUNA <88837420+DANNJUGUNA@users.noreply.github.com>
2023-03-27 19:18:57 +0300

    Update routes.rb
    updated the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-
+  resources :users
+  resources :reviews
+  resources :bookings
   resources :flights
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

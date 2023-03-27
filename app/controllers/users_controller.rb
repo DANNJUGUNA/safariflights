ca386c97b55f4c90c7d1c86be1209ac907a673a1
mknyambura <kariuki.mercie@gmail.com>
2023-03-27 23:42:23 +0300

    update users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
     def index
-        users = Users.all
-        render json: users
+        @users = User.all
+        render json: @users, only: [:username, :email, :password_digest]
     end
 
     def show
-        user = User.find_by(id: params[:id])
-        if user
-            render json: user.as_json
+        @user = User.find_by(id: params[:id])
+        if @user
+            render json: @user.as_json(include: { bookings: { only: [:flight_id, :user_id] }})
         else
             render json: { error: "User Not found" }, status: :not_found
         end
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
-            render json: user.user, status: :created
+            render json: user.bookings, status: :created
         else
             render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
         end

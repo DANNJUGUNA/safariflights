60fd5bb23238284c418df9c9cecd40fff7b63dea
DANNJUGUNA <ndan30071@gmail.com>
2023-03-28 12:52:29 +0300

    updated the navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -4,8 +4,9 @@ const Navbar = () => {
   return (
     <nav className=' mx-4 pb-1 flex justify-between items-center px-5 min-h-[15vh] border-b-2 border-white'  >
         {/* {log} */}
-        <div>
+        <div className=' flex flex-col'>
           <p className='font-bold text-2xl text-white'>SAFARI<span className=' text-[#E99B04] '>FLIGHTS</span></p>
+          <p className='text-white text-sm font-thin'> Customer Satisfaction Is Our Goal</p>
         </div>
         {/* {navigation Linknks} */}
         <div className=' text-white font-semibold flex justify-between gap-6 mr-5' >

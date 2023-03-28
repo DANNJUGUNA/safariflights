f7cb4dfa60b6a24813416b8fb7c6a247c85fcbb1
mknyambura <kariuki.mercie@gmail.com>
2023-03-28 15:16:56 +0300

    responsive navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,24 +1,36 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useRef, useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
 import {Link} from 'react-router-dom'
+
+
 const Navbar = () => {
+  
   return (
-    <nav className=' mx-4 pb-1 flex justify-between items-center px-5 min-h-[15vh] border-b-2 border-white'  >
-        {/* {log} */}
+    // <headers>
+      <nav className=' mx-4 pb-1 flex justify-between items-center px-5 min-h-[15vh] border-b-2 border-white'  >
         <div className=' flex flex-col'>
           <p className='font-bold text-2xl text-white'>SAFARI<span className=' text-[#E99B04] '>FLIGHTS</span></p>
           <p className='text-white text-sm font-thin'> Customer Satisfaction Is Our Goal</p>
         </div>
-        {/* {navigation Linknks} */}
-        <div className=' text-white font-semibold flex justify-between gap-6 mr-5' >
-            <Link to='/'>Home</Link>
-            <Link to='/aboutus'>About us</Link>
-            <Link> Flights</Link>
-            <Link to='/bookings'>Bookings</Link>
-            <Link to='/signup'>Sign up</Link>
-            <Link to='/login'>Log in</Link>
+        {/* {log} */}
+        <div className="text-3xl absolute right-8 top-6 cursor-pointer md-hidden">
+          <button onClick='' className='text-[#E99B04]'><FaTimes/></button>
+          <button onClick='' className='text-[#E99B04]'><FaBars/></button>
         </div>
-    </nav>
+        {/* {navigation Linknks} */}
+        <div className=' text-white font-semibold flex flex-wrap justify-between gap-6 mr-5' >
+          <Link  to='/'>Home</Link>
+          <Link  to='/aboutus'>About us</Link>
+          <Link >Flights</Link>
+          <Link  to='/bookings'>Bookings</Link>
+          <Link  to='/signup'>Sign up</Link>
+          <Link  to='/login'>Log in</Link>
+
+        </div>
+
+      </nav>
+    // </headers>
+    
   )
 }
 

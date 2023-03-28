b290d57eb1fe197c932849be417ded33d450110d
DANNJUGUNA <ndan30071@gmail.com>
2023-03-28 09:25:12 +0300

    created sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Flight.create([
+    {
+        destination: "Mombasa",
+        from: "Nairobi",
+       duration: "45min",
+       departure: "08:00am",
+       cost:10000
+    },
+    {
+        destination: "Kisumu",
+        from: "Nairobi",
+       duration: "1hr",
+       departure: "07:00am",
+       cost:12000
+    },
+    {
+        destination: "Eldoret",
+        from: "Nairobi",
+       duration: "45min",
+       departure: "12:00pm",
+       cost:8000
+    },
+    {
+        destination: "London",
+        from: "Nairobi",
+       duration: "13hr",
+       departure: "09:00am",
+       cost:50000
+    },
+    {
+        destination: "Kisumu",
+        from: "Mombasa",
+       duration: "1hr 20 min",
+       departure: "14:00pm",
+       cost:15000
+    }
+])

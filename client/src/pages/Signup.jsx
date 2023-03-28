37289688310d3a3a473ed598883f82527f4bf0e7
mknyambura <kariuki.mercie@gmail.com>
2023-03-28 13:01:26 +0300

    update signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -9,10 +9,14 @@ const Login = () => {
           <input type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outile-none focus:shadow-outline" placeholder='Username' />
         </div>
         <div className="mt-4">
+          <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email</label>
+          <input type="email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outile-none focus:shadow-outline" placeholder='Email' />
+        </div>
+        <div className="mt-4">
           <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Password</label>
           <input type="password" className="shadow appearance-none border border-blue-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
         </div>
-        <div className="mb-6 mt-4">
+        <div className="mb-6">
           <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Confirm Password</label>
           <input type="password" className="shadow appearance-none border border-blue-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
         </div>

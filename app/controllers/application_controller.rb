7e3802400af1cacf3addb4a2848261f8a939198d
DANNJUGUNA <ndan30071@gmail.com>
2023-03-28 19:21:13 +0300

    added authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,14 @@
 class ApplicationController < ActionController::API
-    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-wrap_parameters format: []
+  include ActionController::Cookies
+  before_action :authorize
 
- def render_not_found_response
-    render json: { error: "#{controller_name.classify} not found" }, status: :not_found
+
+  private
+  def authorize
+      current_user=User.find_by(id: session[:user_id])
+      render json:{error: ["not authorized"]},status: :unauthorized unless current_user
   end
-
-  def render_unprocessable_entity_response(invalid)
-    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
-  end
-
+    
 
 
 end

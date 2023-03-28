f771b91d2115997f78a2d5bb105b1d2abef67d2e
DANNJUGUNA <ndan30071@gmail.com>
2023-03-28 11:44:14 +0300

    added margin and border-bottom

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 const Navbar = () => {
   return (
-    <nav className=' flex justify-between items-center px-5 min-h-[15vh]'  >
+    <nav className=' mx-4 pb-1 flex justify-between items-center px-5 min-h-[15vh] border-b-2 border-white'  >
         {/* {log} */}
         <div>
           <p className='font-bold text-2xl text-white'>SAFARI<span className=' text-[#E99B04] '>FLIGHTS</span></p>
@@ -10,7 +10,7 @@ const Navbar = () => {
         {/* {navigation Linknks} */}
         <div className=' text-white font-semibold flex justify-between gap-6 mr-5' >
             <Link to='/'>Home</Link>
-            <li >About us</li>
+            <Link to='/aboutus'>About us</Link>
             <li> Flights</li>
             <li >Bookings</li>
             <li >Sign up</li>

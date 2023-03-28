88fbae3245866293baae1b654a08ce42bb9aea50
leah wanjiku <leagm22@gmail.com>
2023-03-28 08:15:28 +0300

    updated review model

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,32 @@
 class ReviewsController < ApplicationController
+  def index 
+      review=Review.all
+      render json: review
+  end
 
-    def index 
-        review= Review.all 
-        render json: review 
+  def show
+      review= find_review
+      render json:review
+  end
+  
+  def create
+      render json: Review.create!(review_params)
+    end
+
+    def update
+      review = find_review
+      review.update!(review_params)
     end 
+
+
+  private
+  
+  def find_review
+      Review.find( params[:id])
+   end
+
+   def review_params
+    params.permit( :rating)
+  end
+
 end

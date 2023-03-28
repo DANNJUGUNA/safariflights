cc1b86cc6b8ebee05daa03d9c84de1faa8b8cbc5
DANNJUGUNA <ndan30071@gmail.com>
2023-03-28 19:21:50 +0300

    created the sessions

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,15 +2,17 @@ class SessionController < ApplicationController
    skip_before_action :authorize,only: [:login]
     def login
         user=User.find_by(username: params[:username])
-        if (user $$ user.authenticate(params[:password]))
-            session[:user_id]=user.id
-            render json: { status: :created, loggedin: true, user: user}
-       else
-        render json: {errors: ["wrong username or password"]},status: :unauthorized
-       end
+        ##render json:user
+   if(user && user.authenticate(params[:password]))
+           session[:user_id]=user.id
+          render json: { status: :created, loggedin: true, user: user}
+   else
+       render json: {errors: ["wrong username or password"]},status: :unauthorized
+      end
     end
 
     def logout
-        
+        session.delete :user_id
+        head :no_content
     end
 end

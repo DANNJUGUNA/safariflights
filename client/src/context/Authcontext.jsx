b803e50e4eee2cb0480fade01aac558989df4972
DANNJUGUNA <ndan30071@gmail.com>
2023-03-29 14:02:40 +0300

    added the autherization context

--- client/src/context/Authcontext.jsx
+++ client/src/context/Authcontext.jsx
@@ -1,15 +1,19 @@
 import React ,{useContext,useEffect,useState,createContext}from 'react'
 import {useNavigate} from 'react-router-dom'
-
+import Swal from 'sweetalert2'
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
+
+
     const navigate = useNavigate()
-    // const[user,setUser]=useState()
-    // const [change, setOnChange] = useState(false)
+    const [user, setUser] = useState()
+    const [change, setOnChange] = useState(false)
+
+
     const login = (username, password) =>{
-        fetch(`http://localhost:3000/login`,{
+        fetch('/login',{
             method: "POST",
             headers:{
                 "Content-Type": "application/json"
@@ -21,28 +25,87 @@ const AuthProvider = ({children}) => {
         )
         .then(res=>res.json())
         .then(response=>{
-            // setOnChange(!change)
+             setOnChange(!change)
             if(response.errors)
             {
-                 
-                  console.log("errors")
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Sorry',
+                    text: response.errors
+                })
             }
             else if(response.loggedin)
             {
-             console.log("logged in") 
-                  navigate("/signup")
-
+                Swal.fire({
+                    position: 'center',
+                    icon: 'success',
+                    title: 'LoggedIn successfully!',
+                    showConfirmButton: false,
+                    timer: 1500
+                  }) 
+                  navigate("/aboutus")
+                  
+                  
             }
             else{
-             console.log("something went wrong")
-
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Sorry...',
+                    text: "Something went wrong!",
+                })
             }
             
         })
     }
+    const signup=(username,email,password)=>{
+     fetch('/users',{
+        method:"POST",
+        headers:{
+            'content-type':"application/json"
+        },
+        body: JSON.stringify({
+            username,email,password
+        })
+     }).then(response=>response.json())
+     .then(response=>{
+         setOnChange(!change)
+        if(response.errors)
+        {
+             
+              console.log("errors")
+        }
+        else if(response.loggedin)
+        {
+         console.log("logged in") 
+              navigate("/login")
 
+        }
+        else{
+         console.log("something went wrong")
+
+        }
+        
+    })
+    }
+    useEffect(()=>{
+        fetch("/loggedin",{
+            method: "GET",
+            headers:{
+                "Content-Type": "application/json"
+            },
+        }
+        )
+        .then(res=>res.json())
+        .then(response=>{
+            setUser(response)
+        }
+        )
+    }, [change])
     const contextData={
-        login
+        
+        login,
+        user,
+        signup
     }
   return (
     <AuthContext.Provider value={contextData}>

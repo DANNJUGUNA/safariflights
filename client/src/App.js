50a4d3487034fdfb12e52405ca547d7142140cd6
DANNJUGUNA <ndan30071@gmail.com>
2023-03-29 11:01:19 +0300

    imported the auth context

--- client/src/App.js
+++ client/src/App.js
@@ -7,22 +7,23 @@ import Flight from "./pages/Flight";
 import Aboutus from "./pages/Aboutus";
 import Signup from "./pages/Signup";
 import Booking from './pages/Booking'
+import AuthProvider from "./context/Authcontext";
 
 function App() {
   return (
     <BrowserRouter>
-      
+      <AuthProvider>
       <Routes>
-        
           <Route path="/" element={<Layout/>}>
             <Route index element={<Home/>}/>
             <Route path="/aboutus" element={<Aboutus/>}/>
-            <Route path='booking' element={<Booking/>} />
+            <Route path='/booking' element={<Booking/>} />
             <Route path="/flight" element={<Flight/>}/>
             <Route path="/signup" element={<Signup/>}/>
             <Route path="/login" element={<Login/>}/>
           </Route>
       </Routes>
+      </AuthProvider>
     </BrowserRouter>
     
   );

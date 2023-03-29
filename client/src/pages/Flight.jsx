f8279da102bce3a933cc9f2e7e9d9fff04b6dd7f
DANNJUGUNA <ndan30071@gmail.com>
2023-03-29 18:45:04 +0300

    made the component to dynamic use data from the database

--- client/src/pages/Flight.jsx
+++ client/src/pages/Flight.jsx
@@ -1,99 +1,39 @@
-import React from 'react'
+import React, {useContext } from 'react'
+import { AuthContext } from '../context/Authcontext';
+import { FlightContext } from '../context/Flightscontext';
 
 
-function Flight ({ from, destination, departure, duration, cost }) {
-
-  // let props = {
-  //   flightUrl: '/flights'
-  // }
+function Flight () {
+  const{flights}=useContext(FlightContext)
+  const {user}=useContext(AuthContext)
   return(
-    <div className='flex flex-col-3 gap-6 p-4 w-full h-50vh'>
-      <div className=" bg-white rounded-md shadow-md p-6 w-[30%]">
-        <div className= "col-span-1 gap-2"flex justify-between items-center mb-4>
-          <h3 className="text-xl font-bold">destination:{destination}</h3>
-          <p className="text-gray-600">departure:{departure}</p>
-        </div>
-        <div className="col-span-1 gap-2" flex justify-between items-center mb-4>
-          <h4 className="text-lg">from:{from}</h4>
-          <p className="text-gray-600">duration:{duration}</p>
-        </div>
-        <div className="col-span-1 gap-2">
-          <p className="text-lg font-bold text-green-600">${cost}</p>
-          <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">Book Now</button>
-        </div>
-      </div>
-      <div className=" bg-white rounded-md shadow-md p-6 w-[30%]">
-        <div className="col-span-1 gap-2">
-          <h3 className="text-xl font-bold">destination:{destination}</h3>
-          <p className="text-gray-600">departure:{departure}</p>
-        </div>
-        <div className="col-span-1 gap-2">
-          <h4 className="text-lg">from:{from}</h4>
-          <p className="text-gray-600">duration:{duration}</p>
-        </div>
-        <div className="col-span-1 gap-2">
-          <p className="text-lg font-bold text-green-600">${cost}</p>
-          <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">Book Now</button>
-        </div>
-      </div>
-      <div className=" bg-white rounded-md shadow-md p-6 w-[30%]">
-        <div className="col-span-1 gap-2">
-          <h3 className="text-xl font-bold">destination:{destination}</h3>
-          <p className="text-gray-600">departure:{departure}</p>
-        </div>
-        <div className="col-span-1 gap-2">
-          <h4 className="text-lg">from:{from}</h4>
-          <p className="text-gray-600">duration:{duration}</p>
-        </div>
-        <div className="col-span-1 gap-2">
-          <p className="text-lg font-bold text-green-600">${cost}</p>
-          <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">Book Now</button>
-        </div>
-      </div>
-      <div className=" bg-white rounded-md shadow-md p-6 w-[30%]">
-        <div className="col-span-1 gap-2">
-          <h3 className="text-xl font-bold">destination:{destination}</h3>
-          <p className="text-gray-600">departure:{departure}</p>
-        </div>
-        <div className="col-span-1 gap-2">
-          <h4 className="text-lg">from:{from}</h4>
-          <p className="text-gray-600">duration:{duration}</p>
-        </div>
-        <div className="col-span-1 gap-2">
-          <p className="text-lg font-bold text-green-600">${cost}</p>
-          <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">Book Now</button>
-        </div>
-      </div>
-      <div className=" bg-white rounded-md shadow-md p-6 w-[30%]">
-        <div className="col-span-1 gap-2">
-          <h3 className="text-xl font-bold">destination:{destination}</h3>
-          <p className="text-gray-600">departure:{departure}</p>
-        </div>
-        <div className="col-span-1 gap-2">
-          <h4 className="text-lg">from:{from}</h4>
-          <p className="text-gray-600">duration:{duration}</p>
-        </div>
-        <div className="col-span-1 gap-2">
-          <p className="text-lg font-bold text-green-600">${cost}</p>
-          <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">Book Now</button>
-        </div>
-      </div>
-      <div className=" bg-white rounded-md shadow-md p-6 w-[30%]">
-        <div className="col-span-1 gap-2">
-          <h3 className="text-xl font-bold">destination:{destination}</h3>
-          <p className="text-gray-600">departure:{departure}</p>
-        </div>
-        <div className="col-span-1 gap-2">
-          <h4 className="text-lg">from:{from}</h4>
-          <p className="text-gray-600">duration:{duration}</p>
-        </div>
-        <div className="col-span-1 gap-2">
-          <p className="text-lg font-bold text-green-600">${cost}</p>
-          <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">Book Now</button>
-        </div>
-      </div>
+ <div>
+   <p className='flex items-center justify-center text-2xl mt-2 font-bold text-white underline underline-offset-8'>
+    Available Flights</p>
+    <div className='grid grid-cols-2 lg:grid-cols-4 pt-5 mx-5 mb-3 gap-6'>
+   
+      {
+         flights && flights.map((flight,index)=>(
+            <div key={index} className=" bg-white rounded-3xl shadow-md shadow-[#E99B04] p-6 w-full">
+          
+              <p className="text-lg font-bold">Destination:<span className=' font-normal pl-2 text-xl'>{flight.destination}</span></p>
+              <p className="text-lg font-bold">From: <span className=' font-normal pl-2 text-xl'>{flight.from}</span></p>
       
+              <p className="text-lg font-bold">Departure:<span className=' font-normal pl-2 text-xl'>{flight.departure}</span></p>
+              <p className="text-lg font-bold">Duration: <span className=' font-normal pl-2 text-xl'>{flight.duration} </span></p>
+              <p className="text-lg font-bold mb-2 ">Cost: <span className=' text-green-600 font-normal pl-2 text-lg '>${flight.cost}</span></p>
+              <div className=' flex items-center justify-center'>
+                { 
+                user && user.loggedin?
+              <button type="button" className="focus:outline-none text-black bg-[#E99B04] hover:bg-yellow-500 focus:ring-2 focus:ring-yellow-400 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-1 dark:bg-yellow-600 dark:hover:bg-yellow-600 dark:focus:ring-yellow-700">Book Now</button>
+              :
+              <p>For you to book the flight you have to log in </p>
+                }
+            </div>
+          </div>
+          ))}
     </div>
+  </div>
   );   
 }
 export default Flight

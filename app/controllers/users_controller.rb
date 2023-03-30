e63a7c69db093300fb5d49535b4e9e8411b2064e
minnehkamau <minnieches08@gmail.com>
2023-03-30 18:39:08 +0300

    login and creating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,91 +1,34 @@
 class UsersController < ApplicationController
-    # skip_before_action :authorize, only: [:create] 
-#     def index
-#         users = User.all
-#         render json: users
-#     end
-#     def loggedin
-#         user = User.find_by(id: session[:user_id])
-#         if(user)
-#             render json: {loggedin: true, user: user}
-#          else
-#             render json: {loggedin: false}
-#          end 
-#     end
+   
+skip_before_action :authorized, only: [:create, :login]
 
-#     def create
-#         user = User.create(user_params)
-#         if user.valid?
-#             render json: { "success": "User saved successfully!"}
-#         else
-#             render json: user.errors.messages
-#         end
-#     end
-
-#     # def update
-#     #     user = find_user
-#     #     user.update!(user_params)
-#     #     render json: user
-#     # end
-
-
-  
-        
-#     private
-
-#     def find_user
-#         User.find(params[:id])
-#     end
-
-#     def user_params
-#         params.permit(:username, :email, :password)
-#     end
-
-
-# def create
-#     @user = User.create(user_params)
-
-#     if @user.valid?
-#         token = encode_token({ user_id: @user.id })
-#         render json: { user: @user, token: token }, status: :ok
-#     else
-#         render json: { error: 'Invalid username or password' }, status: :unprocessable_entity
-#     end
-    
-      
-# end
-def create
-    
+  def create
     @user = User.new(user_params)
-  
     if @user.save
       token = encode_token({ user_id: @user.id })
-      render json: { user: @user, token: token }, status: :ok
+      render json: { user: @user.as_json(except: [:created_at, :updated_at]), token: token }
     else
-      render json: { error: @user.errors.full_messages.join(', ') }, status: :unprocessable_entity
+      render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
     end
   end
-  
-def login
-    @user =  User.find_by(username: user_params[:username])
 
-    if @user && @user.authenticate(user_params[:password])
-        token = encode_token({ user_id: @user.id })
-        render json: { user: @user, token: token }, status: :ok
+  def login
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user.as_json(except: [:created_at, :updated_at]), token: token, authorized: true  }
     else
-        render json: { error: 'Incorrect username or password' }, status: :unprocessable_entity
+      render json: { error: 'Invalid username or password' }, status: :unauthorized
     end
-end
+  end
 
-private
+  private
 
-def user_params
-
-    params.require(:user).permit(:username, :email, :password)
-      
-      
-end
-
-  
+  def user_params
+    params.permit(:username, :password, :email)
+  end
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
 end
 
